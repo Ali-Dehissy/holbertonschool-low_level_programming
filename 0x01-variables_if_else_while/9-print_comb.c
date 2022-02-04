168c2104da6f35b88e910c917a2224a659e5b83e
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-04 07:24:05 -0500

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+
+int main(void)
+{
+	int ch = '0';
+	int v = ',';
+	int s = ' ';
+	
+	for (ch = '0' ; ch <= '9' ; ch++)
+	{
+		putchar(ch);
+		if ( ch != '9')
+		{	
+			putchar(v);
+			putchar(s);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}

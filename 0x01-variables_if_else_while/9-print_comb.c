457e185f08bcaaf201986dd567fe6a26fcb94383
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-04 07:26:00 -0500

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,15 +13,15 @@ int main(void)
 	int ch = '0';
 	int v = ',';
 	int s = ' ';
-	
+
 	for (ch = '0' ; ch <= '9' ; ch++)
 	{
-		putchar(ch);
-		if ( ch != '9')
-		{	
-			putchar(v);
-			putchar(s);
-		}
+	putchar(ch);
+	if (ch != '9')
+	{
+	putchar(v);
+	putchar(s);
+	}
 	}
 	putchar('\n');
 

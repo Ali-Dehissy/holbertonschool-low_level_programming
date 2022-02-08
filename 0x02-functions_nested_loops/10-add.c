876200846c05a664de47cb3f3a4f489ca2f66e5f
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-02-08 13:00:59 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * add - prints two integers
+ * @n @a @b: The character to print
+ * Return: On success void.
+ */
+int add(int a, int b);
+{
+	int n;
+
+	n = a + b;
+
+	return (0);
+
+}
+

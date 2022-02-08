1a3d787043253694b51207fde6e226e512432523
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-08 08:58:37 -0500

    prints

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,15 @@
 void print_alphabet_x10(void)
 {
 	char c;
-	int  a = 0;
+	int a;
 
-	while (a <= 0)
+	for (a = 0; a <= 9; a++)
 	{
-	for (c = 'a' ; c <= 'z' ; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 	_putchar(c);
 	}
 	_putchar('\n');
-	return (0);
+	}
 
 }
-

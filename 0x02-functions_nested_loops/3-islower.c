6a422c7dd31e29fdef9372a2f2d67dbdd4832b0e
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-08 10:06:03 -0500

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,14 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * islower - writes the alphabets 10 times in a row
+ * _islower - checks for lowercase
+ * @c: The character to print
  * Return: On success void.
  */
 
 int _islower(int c)
 {
-	if(c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 	return (1);
 	}

41addf4e8ccb52d228c442c70850798674f4fe39
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-08 08:32:58 -0500

    print_alphabets

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,16 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * print_alphabet_x10 - writes the alphabets 10 times in a row 
+ * print_alphabet_x10 - writes the alphabets 10 times in a row
  * Return: On success void.
  */
 
 void print_alphabet_x10(void)
 {
-       int a = 1;
+	int a = 1;
 
-	while (a <= 10) {
+	while (a <= 10)
+	{
 	_putchar(a);
 	a++;
 	}

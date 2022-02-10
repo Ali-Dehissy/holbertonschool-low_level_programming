58b8c694dd43076ec41489538ae95df878711656
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-10 06:03:38 -0500

    morenumbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * more_numbers- looks for uppercase
+ * Return: On success void.
+ */
+void more_numbers(void);
+{
+	int i, a;
+
+	for (i = '0'; i <= '14'; i++)
+	{
+		for (a = '0'; a <= '10'; a++)
+		{
+		
+			_putchar(a)
+		_putchar(a / 10 + '0')
+		
+		}
+	}
+	_putchar('\n');
+}
+}

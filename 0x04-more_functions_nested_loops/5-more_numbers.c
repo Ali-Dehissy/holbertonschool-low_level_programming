aabd0a919619f04bb78c7d0f5e22b12a660434b1
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-10 08:28:59 -0500

    morenumbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,17 +6,19 @@
  */
 void more_numbers(void)
 {
-	int i, a;
+	int a, b;
 
-	for (i = '0'; i <= 14; i++)
-	{
 	for (a = '0'; a <= 10; a++)
 	{
+	for (b = '0'; b <= 14; b++)
+	{
 
-	putchar(a);
-	putchar(a / 10 + '0');
-
+	if (b > 9)
+	{
+		_putchar(b / 10 + 48);
 	}
+		_putchar(b % 10 + 48);
 	}
-	putchar('\n');
+		_putchar('\n');
+	}
 }

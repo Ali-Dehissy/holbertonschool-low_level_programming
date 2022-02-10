c275bf509d658177d366a9a8389b22cca893a811
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-10 06:01:49 -0500

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _isdigit - looks for digit
+ * @c : integer
+ * Return: On success void.
+ */
+int _isdigit(int c);
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}

85f4bd0d0aeac7ef2adc1d47bbde270a2c48fdb2
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-10 07:17:19 -0500

    size

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_square - print hashtags
+ * @size : size
+ * Return: On success void.
+ */
+
+void print_square(int size)
+{
+	char c = '#';
+	int a, b;
+
+	for (a = 0; a < size; a++)
+	{
+	for (b = 0; b < size; b++)
+	{
+		putchar(c);
+	}
+		putchar('\n');
+	}
+
+	return (0);
+}

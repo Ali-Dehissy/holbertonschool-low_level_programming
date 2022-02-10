a684e8e17917d62ee80e967cff1123d13b8cd21a
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-10 07:30:25 -0500

    Add

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,7 +10,7 @@ void print_square(int size)
 {
 	char c = '#';
 	int a, b;
-	
+
 	for (a = 0; a < size; a++)
 	{
 	for (b = 0; b < size; b++)
@@ -19,6 +19,4 @@ void print_square(int size)
 	}
 		_putchar('\n');
 	}
-
-	return (0);
 }

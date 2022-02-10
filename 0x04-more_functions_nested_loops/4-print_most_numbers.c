6d03788adea8987615fdba0a77466c60d07b0261
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-10 06:03:15 -0500

    mostnumbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_most_numbers - looks for uppercase
+ * Return: On success void.
+ */
+void print_most_numbers(void);
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+	if (i != '2' && '4')
+	{
+	_putchar(i);
+	}
+	{
+	_putchar('\n');
+	}
+}
+}

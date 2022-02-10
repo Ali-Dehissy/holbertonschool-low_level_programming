5be75230a776db4389e08a7860def19c569ed1da
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-10 06:02:20 -0500

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * mul - looks for uppercase
+ * @a : integer
+ * @b : integer
+ * Return: On success void.
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (0);
+
+}

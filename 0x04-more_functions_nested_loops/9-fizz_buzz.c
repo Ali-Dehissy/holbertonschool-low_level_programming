a67c96c193051e1fe9e376ae15379689f7cd7bc9
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-11 07:21:22 -0500

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+/**
+ * main - FizzBuzz
+ * Return: On success void.
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 1; a < 100; a++)
+	{
+	if (a % 15 == 0)
+	{
+	printf("FizzBuzz ");
+	}
+	else if (a % 5 == 0)
+	{
+	printf("Buzz ");
+	}
+	else if (a % 3 == 0)
+	{
+	printf("Fizz ");
+	}
+	else
+	{
+	printf("%d ", a);
+	}
+	}
+	printf("Buzz\n");
+	return (0);
+}
+

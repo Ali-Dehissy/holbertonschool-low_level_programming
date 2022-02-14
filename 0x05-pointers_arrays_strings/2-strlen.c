169d6ee48ace4faa25d85455cb6c01c8eb10366e
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-14 04:44:00 -0500

    length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - returns length of string
+ * @s : pointer
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	char c;
+
+	c = strlen(s);
+	{
+	_putchar(c)
+	}
+}

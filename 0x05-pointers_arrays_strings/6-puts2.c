86e03fe82dbe78d59e4365299ae0449fb1adec1e
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-15 06:11:13 -0500

    update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,22 +1,19 @@
 #include "main.h"
 /**
  * puts2 - check the code
- *
+ *@str : pointer
  * Return: Always 0.
  */
 void puts2(char *str)
-{	
+{
 	int i;
+	int j;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-	if (i % 2 == 0)
+	for (j = 0; j > i ; j =+ 2)
 	{
-	_putchar(str[i]);
-	}
-	else
-	{
-	_putchar(str[i] - 1);
+		_putchar(str[j]);
 	}
 	}
 }

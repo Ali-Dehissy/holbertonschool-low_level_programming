ac33c0ab895f3f5a6001a380475233a3fa7ec57c
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-18 04:43:27 -0500

    try

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *rot13 - encodes to rot 13
+ *@a : pointer
+ * Return: Always 0.
+ */
+char *rot13(char *a)
+{
+	int i;
+	int j;
+	char *rot = "nopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; a[i] = '\0'; i++)
+	{
+	for (j = 0; j < 26; j++)
+	if (a[i] == rot[j])
+	{
+
+	}
+	}
+	return (a);
+}
+

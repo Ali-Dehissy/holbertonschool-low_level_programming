b78e42e8f0a4c09fb39476019ee48d9f0dafb063
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-18 07:28:38 -0500

    fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,25 +1,27 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * *cap_string - chnage to uppercase
+ * *cap_string - change to uppercase
  *@s : pointer
  * Return: Always 0.
  */
 char *cap_string(char *s)
 {
+	int a = 0;
 	int i;
-	int j;
-	char  array[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int r = 13;
+	char n[] = {32, '\t', '\n', 59, 33, 63, 34, '(', ')', '{', '}', 44, 46};
 
-	for (i = 0; s[i] ; i++)
+	while (s[a])
 	{
-	for (j = 0; j < 13; j++)
+		i = 0;
+	while (i < r)
 	{
-	if ((i == 0 || s[i - 1] == array[i]) && (s[i] <= 97 && s[i] <= 122))
-	{
-	s[i] -= 32;
+		if ((a == 0 || s[a - 1] == n[i]) && (s[a] >= 97 && s[a] <= 122))
+			s[a] -= 32;
+		i++;
 	}
-	}
+		a++;
 	}
 	return (s);
 }

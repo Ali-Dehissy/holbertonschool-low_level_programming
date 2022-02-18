352bdaf6912431a72a640e9963e0361c7cb58a77
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-18 06:46:31 -0500

    fix

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,18 +7,18 @@
  */
 char *leet(char *a)
 {
-        int i;
-        int j;
-        char *string = "aAeEoOtTlL";
-        char *integer = "4433007711";
+	int i;
+	int j;
+	char *string = "aAeEoOtTlL";
+	char *integer = "4433007711";
 
-        for (i = 0; a[i] != '\0'; i++)
-        {
-        for (j = 0; j < 10; j++)
-                if (a[i] == string[j])
-        {
-                a[i] = integer[j];
-        }
-        }
-        return (a);
+	for (i = 0; a[i] != '\0'; i++)
+	{
+	for (j = 0; j < 10; j++)
+	if (a[i] == string[j])
+	{
+	a[i] = integer[j];
+	}
+	}
+	return (a);
 }

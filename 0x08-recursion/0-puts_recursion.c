6aba171b1352dc15cad55d7513234e6e8cf69f53
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-23 04:28:09 -0500

    update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,6 +6,18 @@
  */
 void _puts_recursion(char *s)
 {
-	puts(s);
-	_putchar('\n');
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	_putchar(s[i]);
+	}
+	_putchar('\0');
+}
+int main ()
+{
+	
+	_puts_recursion(char *s);
+	
+	return (0);
 }

b01cea11a05afb1a26657388f91a0adcd04879c1
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-25 07:04:16 -0500

    trying

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * main - multiplication
+ *@argc : integer
+ *@argv : pointer of arrays
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int sum;
+
+	if (argc == 3)
+	{
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
+	}
+
+	else
+	{
+		printf("Error\n");
+	}
+
+	return (1);
+
+}

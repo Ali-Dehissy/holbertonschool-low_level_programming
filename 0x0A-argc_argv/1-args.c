552572cf107393ed90314b176c7077d148f8147b
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-25 06:34:44 -0500

    trying

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * main - prints the number of arguments 
+ * main - prints the number of arguments
  *@argc : integer
  *@argv : pointer of arrays
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-	   
+
 	printf("%d\n", argc - 1);
 	(void)argv;
-	
+
 	return (0);
 }

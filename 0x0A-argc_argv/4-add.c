e2f48216138c07216ab01887410f874afc171823
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-25 08:51:47 -0500

    update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,13 +19,13 @@ int main(int argc, char *argv[])
 	{
 	for (i = 1; i < argc; i++)
 	{
-		if (!isdigit(argv[i]))
+		if (!isdigit(*argv[i]))
 	{
 		printf("Error\n");
 		return (1);
 	}
 	else
-		sum += atoi(argv[i]);
+		sum += atoi(*argv[i]);
 	}
 		printf("%d\n", sum);
 	}

14fadd90d07501238cecb9595a6c2b65459a3859
ali-dehissy <ali1998dehissy@gmail.com>
2022-02-25 07:54:24 -0500

    trying

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * main - multiplication
+ *@argc : integer
+ *@argv : pointer of arrays
+ * Return: Always 1.
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i;
+
+	if (argv[1] == NULL)
+	{
+		printf("0\n");
+	}
+	else
+	{
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(*argv[i]))
+	{
+		printf("Error\n");
+	return (1);
+	}
+	else
+		sum += atoi(argv[i]);
+	}
+		printf("%d\n", sum);
+	}
+	return (0);
+}

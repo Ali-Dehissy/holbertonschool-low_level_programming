ac94d301204fab781bfa01e1bff08a50ac490839
ali-dehissy <ali1998dehissy@gmail.com>
2022-03-03 07:40:00 -0500

    fix

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *string_nconcat - concatenate
+ *@s1 : pointer
+ *@s2 : pointer
+ *@n : int
+ * Return: Always 0.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int k;
+	char *a;
+
+	if (s1 == NULL)
+	s1 = "";
+	if (s2 == NULL)
+	s2 = "";
+	if (n > strlen(s2))
+		n = strlen(s2);
+	a = malloc(sizeof(char) * (strlen(s1) + n + 1));
+	if (a == NULL)
+		return (NULL);
+	for (k = 0; k < (strlen(s1) + n); k++)
+	{
+	if (k < strlen(s1))
+		a[k] = s1[k];
+	else
+		a[i] = s2[k - strlen(s1)];
+	}
+	a[k] = '\0';
+	return (a);
+
+}

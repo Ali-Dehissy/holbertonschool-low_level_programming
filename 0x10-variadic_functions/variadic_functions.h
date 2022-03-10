8690d70b29cb3b394e7f123738ecea2a2cae9df4
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-03-10 05:13:38 -0800

    requirements

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+#ifndef main_H_
+#define main_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...)
+#endif

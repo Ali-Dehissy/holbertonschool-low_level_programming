885f8842814932711cc1a3397d2c093409f8c449
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-03-17 10:36:13 -0700

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,11 +1,9 @@
 #include "lists.h"
-
 /**
- * print_list - function that prints all the elements of a list_t list
- * @h: head
- * Return: number of nodes
- */
-
+ * print_list - prints all elements
+ * @h : pointer
+ * Return: nodes
+*/
 size_t print_list(const list_t *h)
 {
 	unsigned int node = 0;

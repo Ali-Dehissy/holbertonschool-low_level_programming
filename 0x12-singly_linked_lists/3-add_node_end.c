e05b57134da8f1122ba8bc9a8ed738ea05584593
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-03-18 03:07:56 -0700

    trying

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * *add_node_end - adding a new node at the end
+ * @head : pointer to pointer
+ * @str : const char
+ * Return: new nodes
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *ppt = *head;
+	list_t *newnode = malloc(sizeof(list_t));
+
+	if (newnode == NULL)
+		return (NULL);
+	newnode->str = strdup(str);
+	newnode->len = strlen(str);
+	newnode->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = newnode;
+		return (newnode);
+	}
+	while (ppt->next != NULL)
+	{
+		ppt = ppt->next;
+	};
+
+	ppt->newnode;
+	return (newnode);
+}

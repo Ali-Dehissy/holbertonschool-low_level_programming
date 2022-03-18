9b3b7b686bb1364aace5a56620cd13c1c33c5626
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-03-18 03:19:31 -0700

    update betty style

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,16 +1,15 @@
-include "lists.h"
+#include "lists.h"
 /**
  * free_list - adding a new node
  * @head : pointer to pointer
- * @str : const char
  * Return: new nodes
 */
 void free_list(list_t *head)
 {
 	while (head != NULL)
 	{
-	free (head->str);
-	free (head);
+	free(head->str);
+	free(head);
 	head = head->next;
 	}
 }

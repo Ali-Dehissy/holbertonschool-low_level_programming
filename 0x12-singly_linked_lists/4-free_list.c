9b6d0bb421340e94e366984e900006fcba61f9eb
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-03-18 03:15:44 -0700

    trying

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+include "lists.h"
+/**
+ * free_list - adding a new node
+ * @head : pointer to pointer
+ * @str : const char
+ * Return: new nodes
+*/
+void free_list(list_t *head)
+{
+	while (head != NULL)
+	{
+	free (head->str);
+	free (head);
+	head = head->next;
+	}
+}

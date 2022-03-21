f3482c629f7e39ceb6741e595c4520d9ddf3565f
ali-dehissy <ali1998dehissy@gmail.com>
2022-03-21 07:16:35 -0400

    trying

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_list - prints all elements
+ * @h : pointer
+ * Return: nodes
+*/
+size_t print_listint(const listint_t *h)
+{
+	unsigned int node = 0;
+	
+
+    	while (h != NULL)
+	{
+	printf("%d\n", h->n);
+	h = h->next;
+	node++;
+	}
+	return (node);
+}

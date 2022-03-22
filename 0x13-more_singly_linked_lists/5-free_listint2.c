96a435b0d7cc49150d038b06cbc5246a40ad1ebc
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-03-22 07:31:50 -0700

    trying

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 - free list
+ * @head : pointer to pointer
+ * Return: void
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *a;
+
+	while (*head)
+	{
+		a = *head;
+		*head = (*head)->next;
+		free(a);
+	}
+}

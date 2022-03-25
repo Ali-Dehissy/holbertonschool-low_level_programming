9a695bdfcde65297a40b5758a8d7b4f7a110acb0
ali-dehissy <ali1998dehissy@gmail.com>
2022-03-25 08:30:19 -0400

    fixed missclick

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -7,13 +7,12 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	int clear_bit(unsigned long int *n, unsigned int index)
 
 	if (index > (sizeof(unsigned long int) * 8))
 
 		return (-1);
 
-	n &= ~(1 << x);
+	*n &= ~(1 << index);
 
 	return (1);
 }

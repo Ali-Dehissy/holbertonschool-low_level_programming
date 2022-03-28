729cebc6953b50c55c85f4b82447f5a11954b5ad
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-03-28 11:19:37 -0700

    fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,11 +18,11 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filename == NULL)
 		return (0);
 
-	fd = open("filename", O_RDONLY);
-	
+	fd = open(filename, O_RDONLY);
+
 		if (fd == -1)
 			return (0);
-	
+
 
 	fz = read(fd, a, letters);
 

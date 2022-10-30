0033b02de9c319808156fc922298f0e8050c8b18
Ali-Dehissy <ali1998dehissy@gmail.com>
2022-10-30 13:44:06 +0100

    update

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -1,29 +1,28 @@
 #include "hash_tables.h"
+
 /**
  * hash_table_delete - deletes a hash table
-  * @ht: pointer to hash table data struct
+ *
+ * @ht: pointer to hash table data structure
  * Return: void
  */
-void hash_table_delete(hash_table_t *ht)
-{
-	unsigned int i = 0;
-	hash_node_t *head = NULL;
-	hash_node_t *tmp = NULL;
+void hash_table_delete(hash_table_t * ht) {
+   hash_node_t * head = NULL;
+   hash_node_t * tempo = NULL;
+   unsigned int j = 0;
 
-	if (!ht)
-		return;
-	for (i = 0; i < ht->size; i++)
-	{
-		head = ht->array[i];
-		while (head != NULL)
-		{
-			tmp = head->next;
-			free(head->key);
-			free(head->value);
-			free(head);
-			head = tmp;
-		}
-	}
-	free(ht->array);
-	free(ht);
+   if (!ht)
+      return;
+   for (j = 0; j < ht -> size; j++) {
+      head = ht -> array[j];
+      while (head != NULL) {
+         tempo = head -> next;
+         free(head -> key);
+         free(head -> value);
+         free(head);
+         head = tempo;
+      }
+   }
+   free(ht -> array);
+   free(ht);
 }
